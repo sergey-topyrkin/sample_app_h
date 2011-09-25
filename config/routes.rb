52e37d43a016b79d20c1790862b79c0bbcacb13b
sergey.topyrkin <sergey.topyrkin@gmail.com>
2011-09-25 22:40:42 +0400

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 SampleAppH::Application.routes.draw do
-  get "pages/home"
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  match '/home',    :to => 'pages#home'
 
-  get "pages/contact"
-
-  get "pages/about"
+  root :to => 'pages#home'
+  
+  #get "pages/home"
+  #get "pages/contact"
+  #get "pages/about"
 end
